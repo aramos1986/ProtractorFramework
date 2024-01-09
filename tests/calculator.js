75fcd5b2e735cfc1412014f4a49c670e4c317682
aramos1986 <a.ramos@arcanys.com>
2024-01-09 15:23:00 +0800

    updated calc 3

--- tests/calculator.js
+++ tests/calculator.js
@@ -15,6 +15,19 @@ describe('demo calculator tests', function () {
 
         browser.sleep(5000)
     });
+    it('subtraction test', function () {
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo1();
+
+        homepage.verifyResult('7');
+
+        browser.sleep(5000)
+    });
 
 
 })
